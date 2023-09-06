dd393211cecdc30613bddfb6ceb0813c6d76809a
Guilherme Wanderley <pireswanderley12@gmail.com>
2023-09-05 21:11:38 -0300

    bad solution

--- src/Aplication/Program.java
+++ src/Aplication/Program.java
@@ -12,47 +12,40 @@ public class Program {
 
 	public static void main(String[] args) throws ParseException {
 		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner (System.in);
+		Scanner sc = new Scanner(System.in);
 		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-		
+
 		System.out.println("Room number: ");
 		int roomNumber = sc.nextInt();
 		System.out.println("Check-in date (dd/MM/yyyy): ");
 		LocalDate checkIn = LocalDate.parse(sc.next(), dtf);
 		System.out.println("Check-out date (dd/MM/yyyy): ");
 		LocalDate checkOut = LocalDate.parse(sc.next(), dtf);
-		
+
 		if (!checkOut.isAfter(checkIn)) {
 			System.out.println("Erro in reservation: Check-out date must be after check-in date ");
-		}else {
-				Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
-				System.out.println("Reservation: "+ reservation.toString());
-				
-				System.out.println("");
-				System.out.println("Enter data to update the reservation: ");
-				System.out.println("Check-in date (dd/MM/yyyy): ");
-				checkIn = LocalDate.parse(sc.next(), dtf);
-				System.out.println("Check-out date (dd/MM/yyyy): ");
-				checkOut = LocalDate.parse(sc.next(), dtf);
-				
-				reservation.updateDates(checkIn, checkOut);
-				System.out.println("Reservation: "+ reservation.toString());
-				
-				LocalDate now = LocalDate.now();
-				if (checkIn.isBefore(now) || checkOut.isBefore(now)) {
-					System.out.println("Error in reservation: Reservation dates for update must be future dates");
-				}
-				else if (!checkOut.isAfter(checkIn)) {
-					System.out.println("Error in reservation: Check-out date must be after check-in date");
-				}
-				else {
-					reservation.updateDates(checkIn, checkOut);
-					System.out.println("Reservation: " + reservation);
-				}
+		} else {
+			Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
+			System.out.println("Reservation: " + reservation.toString());
+
+			System.out.println("");
+			System.out.println("Enter data to update the reservation: ");
+			System.out.println("Check-in date (dd/MM/yyyy): ");
+			checkIn = LocalDate.parse(sc.next(), dtf);
+			System.out.println("Check-out date (dd/MM/yyyy): ");
+			checkOut = LocalDate.parse(sc.next(), dtf);
+
+			reservation.updateDates(checkIn, checkOut);
+			System.out.println("Reservation: " + reservation.toString());
+
+			String erro = reservation.updateDates(checkIn, checkOut);
+			if (erro != null) {
+				System.out.println("Erro in reservation: " + erro);
+			} else {
+				System.out.println("Reservation: " + reservation);
 			}
-		
-		
-		
+		}
+
 		sc.close();
 	}
 
--- src/Model/Entities/Reservation.java
+++ src/Model/Entities/Reservation.java
@@ -9,10 +9,9 @@ public class Reservation {
 	private Integer roomNumber;
 	private LocalDate checkIn;
 	private LocalDate checkOut;
-	
+
 	DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-	
-	
+
 	public Reservation(Integer roomNumber, LocalDate checkIn, LocalDate checkOut) {
 		this.roomNumber = roomNumber;
 		this.checkIn = checkIn;
@@ -34,26 +33,27 @@ public class Reservation {
 	public LocalDate getCheckOut() {
 		return checkOut;
 	}
-	
+
 	public long duration() {
 		long diff = checkOut.getDayOfYear() - checkIn.getDayOfYear();
 		return diff;
 	}
-	
-	public void	updateDates(LocalDate checkIn, LocalDate chechOut) {
+
+	public String updateDates(LocalDate checkIn, LocalDate chechOut) {
+		LocalDate now = LocalDate.now();
+		if (checkIn.isBefore(now) || checkOut.isBefore(now)) {
+			return "Error in reservation: Reservation dates for update must be future dates";
+		}
+		if (!checkOut.isAfter(checkIn)) {
+			return "Error in reservation: Check-out date must be after check-in date";
+		}
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
+		return null;
 	}
-	
+
 	public String toString() {
-		return "Room "+
-			+ roomNumber
-			+ ", check-in "
-			+ dtf.format(checkIn)
-			+ ", check-out "
-			+ dtf.format(checkOut)
-			+ ", "
-			+ duration()
-			+ " nights";
+		return "Room " + +roomNumber + ", check-in " + dtf.format(checkIn) + ", check-out " + dtf.format(checkOut)
+				+ ", " + duration() + " nights";
 	}
 }
